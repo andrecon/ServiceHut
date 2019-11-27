2e0c26d5b779688e933c96813d10be9cb2e22ac7
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-11-26 17:17:14 -0800

    Fixed Image display

--- tke_project/myapp/static/js/philanScript.js
+++ tke_project/myapp/static/js/philanScript.js
@@ -2,14 +2,24 @@
 var modal = document.getElementById("myModal");
               
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
+// var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+
+function display(element)
+{
+    var img = document.getElementById(element.id);
+
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
 }
+
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
