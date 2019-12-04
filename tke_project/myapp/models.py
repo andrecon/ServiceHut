a8f716673adc7419f6b510c481dd44838030eac1
Andres Contreras <andrescontreras@Andress-MacBook-Air-2.local>
2019-12-03 17:16:31 -0800

    Fixed philanthropy section. Fixed Fontawesome icons

--- tke_project/myapp/models.py
+++ tke_project/myapp/models.py
@@ -2,8 +2,7 @@ from django.db import models
 from datetime import datetime
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+
 
 # Create your models here.
 
@@ -13,7 +12,7 @@ class Event(models.Model):
     published_date = models.DateTimeField(auto_now_add=True, blank=True) #Current date
     created_date = models.DateField()  #Future Date
     max_volunteers = models.IntegerField(default=1)
-    current_volunteers = models.IntegerField(default=1)
+    # current_volunteers = models.IntegerField(default=1)
     cover = models.ImageField(upload_to='event_images/')
     status = models.BooleanField(default=True)
     # post_author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
@@ -23,19 +22,24 @@ class Event(models.Model):
     state = models.CharField(max_length=500)
     def __str__(self):
         return self.title
+    
+    def ready(self):
+        post_save.connect(on_batch_child_saving, sender=Volunteer)
 
 class Volunteer(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone_number = PhoneNumberField(default='+1')
 
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.email
 
     class Meta:
         ordering = ['name', 'email', 'phone_number', 'event']
+
+
 
 
 class Gallery(models.Model):
@@ -69,3 +73,9 @@ class RUSH(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# class UserProfile(models.Model):
+#     url = models.URLField()
+#     phone_numer = models.PhoneNumberField()
+#     user = models.ForeignKey(User, unique=True)
--- tke_project/myapp/static/js/philanScript.js
+++ tke_project/myapp/static/js/philanScript.js
@@ -28,3 +28,67 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+
+// function openList1(element) {
+//   var list = document.getElementById(element.id);
+//   // console.log($(this));
+//   // var list = $(this).closest('p').next("ol").attr('id')
+//   console.log(list);
+
+//   if (list.style.display == "none"){
+//       list.style.display = "block";
+//   }else{
+//       list.style.display = "none";
+//   }
+// }
+
+
+$(function() {
+  $('p.edit').click(function(e) {
+    e.preventDefault();
+    // console.log($(this).text())
+    $(this).text(($(this).text() == 'Volunteer List: (Show Volunteers)') ? 'Volunteer List:' : 'Volunteer List: (Show Volunteers)').fadeIn();
+    // var list = $(this).next("ol").attr("id");
+    var list = $(this).next("spam").next("ol").attr("id");
+
+    // var total = $(this).next("ol").next("spam").attr("id");
+    var total = $(this).next("spam").attr("id");
+
+    console.log(total);
+    var theList = document.getElementById(list).getElementsByTagName("li");
+    var numberOfItems = theList.length;
+    console.log(numberOfItems);
+
+    document.getElementById(total).innerHTML = "Current Number of Volunteers: " + numberOfItems.toString();
+    // $(total).text(numberOfItems).fadeIn();
+
+    hide(list, total);
+
+  });
+});
+
+function hide(element, total)
+{
+  var list = document.getElementById(element);
+
+  var elementData = document.getElementById(total);
+
+  if (elementData.style.display == "none"){
+    elementData.style.display = "block";
+  }else{
+    elementData.style.display = "none";
+  }
+
+
+  console.log(list);
+  if (list.style.display == "none"){
+    list.style.display = "block";
+  }else{
+    list.style.display = "none";
+  }
+}
+
+(function(){
+  // some code…
+ })();
